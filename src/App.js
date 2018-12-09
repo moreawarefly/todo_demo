dd648a90e91bd0d56a7e7600a2f1d1d5e6ee80f0
Marcin Skawiński <marcin@moreawarefly.com>
2018-12-09 22:13:08 +0100

    step1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       enemies: ['cat', 'dog', 'baby'],
+//     };
+//   }
+//   removeEnemy = (enemyToRemove) => {
+//     this.setState({ enemies: this.state.enemies.filter(enemy => enemy !== enemyToRemove)});
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           this is where the header was
+//         </header>
+//         <div className="enemies-list">
+//           {this.state.enemies.map(enemy => (
+//             <button onClick={() => this.removeEnemy(enemy)} key={enemy}>
+//               {enemy}
+//             </button>
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+function App() {
+  const [enemies, setEnemies] = useState([
+    'cat',
+    'dog',
+    'baby'
+  ]);
+
+  function removeEnemy(enemyToBeRemoved) {
+    setEnemies(
+      enemies.filter(enemy => enemy !== enemyToBeRemoved)
     );
   }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        this is where the header was
+      </header>
+      <div className="enemies-list">
+        {enemies.map(enemy => (
+          <button onClick={() => removeEnemy(enemy)} key={enemy}>
+            {enemy}
+          </button>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default App;
